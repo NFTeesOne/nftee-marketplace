226a76d2ef7c3855213275e05a39a64562533a10
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-24 16:26:28 -0400

    Added NatSpec comments

--- contracts/INFTeeMarketplace.sol
+++ contracts/INFTeeMarketplace.sol
@@ -9,11 +9,13 @@ pragma solidity ^0.8.19;
 
 /**
  * @title Buy It Now Interface
- * @notice Interface for listing tokens at a set price
+ * @notice Interface for listing NFTees at a set price
+ * @dev This is a subset of the NFTee Marketplace interface
  */
 interface IBuyItNow {
     enum Status { Open, Sold, Removed }
 
+    /// @dev Parameters for listing an NFTee
     struct ListingParameters {
         address nfteeContract;
         uint256 tokenId;
@@ -25,6 +27,7 @@ interface IBuyItNow {
         uint128 endTimestamp;
     }
 
+    /// @dev Data contained in a listing
     struct Listing {
         uint256 listingId;
         address seller;
@@ -39,6 +42,7 @@ interface IBuyItNow {
         Status status;
     }
 
+    /// @dev Emitted when an NFTee is listed
     event NFTeeListed(
         uint256 indexed listingId,
         address indexed seller,
@@ -46,6 +50,7 @@ interface IBuyItNow {
         Listing listing
     );
 
+    /// @dev Emitted when an NFTee is updated
     event NFTeeUpdated(
         uint256 indexed listingId,
         address indexed seller,
@@ -53,6 +58,7 @@ interface IBuyItNow {
         Listing listing
     );
 
+    /// @dev Emitted when an NFTee is sold
     event NFTeeSold(
         uint256 indexed listingId,
         address indexed seller,
@@ -63,24 +69,67 @@ interface IBuyItNow {
         uint256 totalPrice
     );
 
+    /// @dev Emitted when an NFTee is removed
     event NFTeeRemoved(
         uint256 indexed listingId,
         address indexed seller
     );
 
+    /**
+     * @notice List an NFTee for sale at a set price
+     * @param _listingParameters Parameters for listing an NFTee
+     * @return listingId ID of the listing
+     */
     function listNFTee(ListingParameters calldata _listingParameters) external returns (uint256 listingId);
 
+    /**
+     * @notice Update an NFTee listing
+     * @param _listingId ID of the listing
+     * @param _listingParameters Parameters for listing an NFTee
+     */
     function updateNFTee(uint256 _listingId, ListingParameters calldata _listingParameters) external;
 
+    /**
+     * @notice Buy an NFTee
+     * @param _listingId ID of the listing
+     * @param _sendTo Address to send the NFTee to
+     * @param _quantity Quantity of the NFTee to buy
+     * @param _totalPrice Total price of the purchase
+     */
     function buyNFTee(uint256 _listingId, address _sendTo, uint256 _quantity, uint256 _totalPrice) external payable;
 
+    /**
+     * @notice Remove an NFTee listing
+     * @param _listingId ID of the listing
+     */
     function removeNFTee(uint256 _listingId) external;
 
+    /**
+     * @notice Get the total number of listings
+     * @return totalListings Total number of listings
+     */
     function getTotalListings() external view returns (uint256);
 
+    /**
+     * @notice Get a listing
+     * @param _listingId ID of the listing
+     * @return listing Listing data
+     */
     function getListing(uint256 _listingId) external view returns (Listing memory listing);
 
+    /**
+     * @notice Get all active listings
+     * @param _startId ID of the first listing
+     * @param _endId ID of the last listing
+     * @return listings Array of listings
+     */
     function getAllActiveListings(uint256 _startId, uint256 _endId) external view returns (Listing[] memory listings);
 
+    /**
+     * @notice Get all historical listings
+     * @param _startId ID of the first listing
+     * @param _endId ID of the last listing
+     * @return listings Array of listings
+     */
     function getAllHistoricalListings(uint256 _startId, uint256 _endId) external view returns (Listing[] memory listings);
 }
